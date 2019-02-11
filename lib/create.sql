ad927402676da8d6fb5594fc6ae68d0916e6b230
rachelkathleen <rachel.hawa@gmail.com>
2019-02-10 19:08:34 -0600

    Done.

--- lib/create.sql
+++ lib/create.sql
@@ -1,3 +1,19 @@
-#testing
+CREATE TABLE projects (
+id INTEGER PRIMARY KEY,
+title TEXT,
+category TEXT,
+funding_goal INTEGER,
+start_date TEXT,
+end_date TEXT
+);
 
-#second test
+CREATE TABLE users (
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER);
+
+CREATE TABLE pledges (
+id INTEGER PRIMARY KEY,
+user_id INTEGER,
+project_id INTEGER,
+amount INTEGER);
